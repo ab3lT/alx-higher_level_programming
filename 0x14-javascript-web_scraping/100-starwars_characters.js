48cab253d895a266c49a7a2c693028b601c5743d
ab3lT <72024981+ab3lT@users.noreply.github.com>
2022-04-13 00:13:45 +0300

    parcing to utf-8

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -7,14 +7,14 @@ req.get(url + id, function (error, res, body) {
   if (error) {
     console.log(error);
   }
-  const data = JSON.parse(body, 'utf 8');
+  const data = JSON.parse(body, 'utf-8');
   const dd = data.characters;
   for (const i of dd) {
     req.get(i, function (error, res, body1) {
       if (error) {
         console.log(error);
       }
-      const data1 = JSON.parse(body1, 'utf 8');
+      const data1 = JSON.parse(body1, 'utf-8');
       console.log(data1.name);
     });
   }
--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,7 +3,7 @@ const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
   if (!error) {
-    const characters = JSON.parse(body, 'utf 8').characters;
+    const characters = JSON.parse(body, 'utf-8').characters;
     printCharacters(characters, 0);
   }
 });
