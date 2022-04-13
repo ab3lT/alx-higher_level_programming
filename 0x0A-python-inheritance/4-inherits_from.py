1540ac3a7788e1bb433978ab6baffaae80ac684e
ab3lT <72024981+ab3lT@users.noreply.github.com>
2022-04-13 17:47:29 +0300

    Inherif from

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3
 """ check type object"""
 
+"""Only sub class of.
+Author: Kanneh
+
+"""
+
 
 def inherits_from(obj, a_class):
     """ check type object"""
+    """A function that returns True if the object is
+    an instance of a class that inherited (directly or indirectly)
+    from the specified class;
+    otherwise False.
+
+    Args:
+        obj(any): object of the class
+        a_class(type): describes the class
+
+    """
     if isinstance(obj, a_class) and type(obj) != a_class:
         return True
     else:
